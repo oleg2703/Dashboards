7f42062ae5e396d08f71668eb14a61f71a7e0a95
Helg4s <oleg2703lebid@gmail.com>
2026-06-07 23:32:28 +0300

    structur -dashboard

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,20 @@
 import { Link } from '@tanstack/react-router'
 import ThemeToggle from './ThemeToggle'
+import { Bell, CirclePlus, Gift } from 'lucide-react'
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between border-b bg-background px-4 py-2">
-     <div className="text-xl font-bold">Logo</div>
-     <ul className="flex items-center place-content-end gap-4 py-4">
-      <ThemeToggle />
-      <li>Login </li>
-      <li>Sign Up</li>
-      <li>Profile</li>
+    <header className="w-full flex items-center justify-between border-b bg-background px-4 py-2">
+     
+     <ul className="flex items-left place-content-end gap-4 py-4">
+     <li><input type="text" placeholder="Search..." /></li>
      </ul>
+     <ul className="flex items-right gap-4">
+        <li><Bell /></li>
+        <li><CirclePlus /></li>
+        <li><Gift /></li>
+        <li><ThemeToggle /></li>
+      </ul>
     </header>
   )
 }
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,28 +1,48 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Dashboard from '../components/layout/Dashboard'
 import { StatCard } from '../components/dashboard/StatCard'
+import  Header  from '../components/layout/Header'
 
 export const Route = createFileRoute('/dashboard')({
   component: () => (
     <main className="flex w-full h-screen">
     <Dashboard>
+      
+      <div className=" w-full">
+        <Header />
+        <div className="w-full flex items-center justify-between px-4 py-2">
+          <h1>Dashboard</h1> 
+          <ul className='flex gap-4'>
+            <li>Overview</li>
+            <li>Analytics</li>
+            <li>Reports</li>
+          </ul>
+        </div>
+         
       <div className="flex gap-4">
-         <StatCard 
-        title="Revenue" 
-        value="$24,580" 
-        change="+12%" 
-      />
-       <StatCard 
-        title="Revenue" 
-        value="$24,580" 
-        change="+12%" 
-      />
-       <StatCard 
-        title="Revenue" 
-        value="$24,580" 
-        change="+12%" 
-      />
+          <StatCard 
+          title="Total Revenue" 
+          value="$24,580" 
+          change="+12%" 
+        />
+        <StatCard 
+          title="Total Orders" 
+          value="154" 
+          change="+8 this week" 
+        />
+        <StatCard 
+          title="Customers" 
+          value="1,248" 
+          change="+24 this week" 
+        />
+          <StatCard 
+          title="Products" 
+          value="34" 
+          change="5 low stock" 
+        />
+        </div>
       </div>
+      
      
     </Dashboard>
     </main>
