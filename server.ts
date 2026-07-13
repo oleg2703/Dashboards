48becb68daf7f9b6b6c88bef6a27275381a61841
Helg4s <oleg2703lebid@gmail.com>
2026-07-13 11:42:37 +0300

    deploy-fix

---
+++ server.ts
@@ -0,0 +1,16 @@
+
+// eslint-disable-next-line import/no-commonjs
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+const port = process.env.PORT || 18013
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on ${port}`)
+})
